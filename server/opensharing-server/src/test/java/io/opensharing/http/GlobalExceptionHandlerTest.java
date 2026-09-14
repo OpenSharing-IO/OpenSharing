dc67b00853711dad5490c256820fb15858ee797a
Sun Cao <sun.cao+data@databricks.com>
2026-09-14 11:09:33 -0700

    Return protocol error bodies for HTTP failures.

---
+++ server/opensharing-server/src/test/java/io/opensharing/http/GlobalExceptionHandlerTest.java
@@ -0,0 +1,31 @@
+package io.opensharing.http;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(
+    properties = {
+      "spring.datasource.url=jdbc:h2:mem:http-errors;DB_CLOSE_DELAY=-1",
+      "opensharing.catalog.type=local",
+      "opensharing.catalog.local.file=classpath:local-catalog.yml"
+    })
+@AutoConfigureMockMvc
+class GlobalExceptionHandlerTest {
+
+  @Autowired private MockMvc mvc;
+
+  @Test
+  void answersUnknownRoutesWithTheProtocolErrorBody() throws Exception {
+    mvc.perform(get("/no-such-route"))
+        .andExpect(status().isNotFound())
+        .andExpect(jsonPath("$.errorCode").value(ErrorCodes.RESOURCE_DOES_NOT_EXIST))
+        .andExpect(jsonPath("$.message").value("the endpoint does not exist"));
+  }
+}
