b358dc2dd30dc1e2fa1985b19405fd649d7340e4
Sun Cao <sun.cao+data@databricks.com>
2026-09-25 15:08:09 -0700

    Point protocol schema tests at the local catalog YAML.

--- server/opensharing-server/src/test/java/io/opensharing/asset/schema/ProtocolSchemaControllerTest.java
+++ server/opensharing-server/src/test/java/io/opensharing/asset/schema/ProtocolSchemaControllerTest.java
@@ -12,12 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 
-@SpringBootTest(
-    properties = {
-      "spring.datasource.url=jdbc:h2:mem:protocol-schemas;DB_CLOSE_DELAY=-1",
-      "opensharing.catalog.type=local",
-      "opensharing.catalog.local.file=classpath:local-catalog.yml"
-    })
+@SpringBootTest(properties = "spring.datasource.url=jdbc:h2:mem:protocol-schemas;DB_CLOSE_DELAY=-1")
 class ProtocolSchemaControllerTest extends ProtocolApiSupport {
 
   @Test
